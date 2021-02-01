ac7e822b8c08f07b464075d29278fe92efaa0f8b
Bob <igor.strelbitsky@gmail.com>
2021-02-01 17:52:06 +0200

    t

---
+++ src/com/production/Main.java
@@ -0,0 +1,11 @@
+package com.production;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int q = 3;
+        int y = 3;
+        int t = 4;
+        int r = 4;
+    }
+}
